2aa0a2106bdcc4bbe8417ba3bc119a57e5291773
NeroInu <1204584@abertay.ac.uk>
2015-01-23 22:26:18 +0000

    New Scripts

---
+++ Project/Assets/Scripts/GameVariables.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameVariables : MonoBehaviour {
+
+	public static int lightWizHealth = 100;
+	public static int lightWizMana = 100;
+	public static int darkWizHealth = 100;
+	public static int darkWizMana = 100;
+
+	// Use this for initialization
+	void Start () {
+
+		lightWizHealth = 100;
+		lightWizMana = 100;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
