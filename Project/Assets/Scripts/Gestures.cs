08eb7de6d1325d27cccbec9fb15796404d435507
johnjoemcbob <mcormack1994@hotmail.co.uk>
2015-01-24 00:04:38 +0000

    Wand detection
    Displays wand sprite when the player's tool is detected over the Leap

--- Project/Assets/Scripts/Gestures.cs
+++ Project/Assets/Scripts/Gestures.cs
@@ -19,26 +19,40 @@ public class Gestures : MonoBehaviour
 
 	void Update()
     {
-        // Find any gestures in this frame
-        GestureList gestures = LeapController.leap_controller_.Frame().Gestures();
-        foreach ( Gesture gesture in gestures )
+        // Ensure the editor has LeapController set
+        if (LeapController)
         {
-            // Gesture exists
-            if ( gesture.IsValid )
+            // Get the actual Leap controller, with useful data
+            Controller controller = LeapController.leap_controller_;
+            if (controller.IsConnected)
             {
-                // Gesture type recognition
-                if ( gesture.Type == Gesture.GestureType.TYPE_CIRCLE ) // Circle
+                // Get the status of the Leap Motion this frame
+                Frame frame = controller.Frame();
+                if (frame.IsValid)
                 {
-                    CircleGesture circlegesture = new CircleGesture( gesture );
-                }
-                else if ( gesture.Type == Gesture.GestureType.TYPE_SWIPE ) // Swipe, line
-                {
-                    SwipeGesture swipegesture = new SwipeGesture( gesture );
-                    if ( Mathf.Abs( swipegesture.Direction.y ) > 0.5f ) // Moving up and down
+                    // Find any gestures in this frame
+                    GestureList gestures = frame.Gestures();
+                    foreach (Gesture gesture in gestures)
                     {
-                        if ( Mathf.Abs( swipegesture.Direction.z ) < 0.8f ) // Not moving much left and right
+                        // Gesture exists
+                        if (gesture.IsValid)
                         {
-                            
+                            // Gesture type recognition
+                            if (gesture.Type == Gesture.GestureType.TYPE_CIRCLE) // Circle
+                            {
+                                CircleGesture circlegesture = new CircleGesture(gesture);
+                            }
+                            else if (gesture.Type == Gesture.GestureType.TYPE_SWIPE) // Swipe, line
+                            {
+                                SwipeGesture swipegesture = new SwipeGesture(gesture);
+                                if (Mathf.Abs(swipegesture.Direction.y) > 0.3f) // Moving up and down
+                                {
+                                    //if (Mathf.Abs(swipegesture.Direction.z) < 0.8f) // Not moving much left and right
+                                    {
+                                        print("CAAAAST");
+                                    }
+                                }
+                            }
                         }
                     }
                 }
@@ -47,19 +61,19 @@ public class Gestures : MonoBehaviour
 
         // Find all the tools in the scene
         ToolModel[] tools = GameObject.FindObjectsOfType<ToolModel>();
-        foreach ( ToolModel tool in tools )
+        foreach (ToolModel tool in tools)
         {
             // TODO: Crossproduct for direction of tool??
             // Find center x position of the wand (Tip position minus half length in wand direction)
-            float wandcenterx = tool.GetLeapTool().TipPosition.x - ( tool.GetLeapTool().Direction.x * tool.GetLeapTool().Length / 2 );
-            if ( wandcenterx < CenterX ) // Player 1 (array 0)
+            float wandcenterx = tool.GetLeapTool().TipPosition.x - (tool.GetLeapTool().Direction.x * tool.GetLeapTool().Length / 2);
+            if (wandcenterx < CenterX) // Player 1 (array 0)
             {
                 Player_Tool[0] = tool;
             }
-            else if ( wandcenterx > CenterX ) // Player 2 (array 1)
+            else if (wandcenterx > CenterX) // Player 2 (array 1)
             {
                 Player_Tool[1] = tool;
             }
         }
-	}
+    }
 }
---
+++ Project/Assets/Scripts/PlayerWand.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerWand : MonoBehaviour
+{
+    public int Player = 1; // The player this wand belongs to
+    public Gestures GestureManager;
+
+	void Start()
+    {
+	
+	}
+
+	void Update()
+    {
+        // Ensure GestureManager has been set in the editor
+	    if (GestureManager)
+        {
+            // Player is in the right boundaries
+            if ((Player >= 1) && (Player <= 2))
+            {
+                bool HasTool = false;
+                // Player has a tool
+                if (GestureManager.Player_Tool[Player-1])
+                {
+                    // Player's tool is still valid (may be removed from scene, etc)
+                    if (GestureManager.Player_Tool[Player-1].GetLeapTool().IsValid)
+                    {
+                        HasTool = true;
+                    }
+                    else
+                    {
+                        HasTool = false;
+                    }
+                }
+                else
+                {
+                    HasTool = false;
+                }
+                if (HasTool)
+                {
+                    GetComponent<SpriteRenderer>().color = new Color(255, 255, 255, 255);
+                }
+                else
+                {
+                    GetComponent<SpriteRenderer>().color = new Color(255, 255, 255, 0);
+                }
+            }
+        }
+	}
+}
