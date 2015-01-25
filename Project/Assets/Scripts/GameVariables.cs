37c1a56f7a6ed85c516cafcd8a9143324b690a0b
NeroInu <1204584@abertay.ac.uk>
2015-01-25 13:02:56 +0000

    boop

--- Project/Assets/Scripts/GameVariables.cs
+++ Project/Assets/Scripts/GameVariables.cs
@@ -13,10 +13,6 @@ public class GameVariables : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 
-		lightWizHealth = 100;
-		lightWizMana = 100;
-		darkWizHealth = 100;
-		darkWizMana = 100;
 
 	}
 	
